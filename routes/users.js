00326fee86e57e55c22ad76cb3322674308836ac
AJ Hagemann <54418965+AJ-Hagemann@users.noreply.github.com>
2021-10-27 15:39:45 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+variable = 0;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  variable ++
+  res.send('User accesses are: ' + variable);
 });
+
+
 
 module.exports = router;
